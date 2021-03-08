25bb4dc3529eec21490ef1a94373e066a6c56424
Dukhart <JDonDuncan@gmail.com>
2021-03-08 12:04:45 -0700

    Physics and UI update
    Rockets no longer get stuck.
    Ball & Wheels now use a physics material.
    Rockets can be assigned an offset allowing for staggered rockets.
    UI now uses text mesh Pro and is properly anchored.

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    public float speed = 1;
+    [SerializeField] float speed = 1;
+    [SerializeField] float distance = 12;
+    [SerializeField] Vector2 direction = Vector2.right;
+    [SerializeField] Vector2 offset = Vector2.zero;
+    private Vector2 endPos;
     // Start is called before the first frame update
     void Start()
     {
+        endPos = (Vector2)transform.position + offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed);
-        if (Mathf.Abs(transform.position.x) > 12)
+        transform.Translate(direction * Time.deltaTime * speed);
+        if (Vector2.Distance(transform.position, endPos) >= distance)
         {
             transform.Rotate(new Vector2(0, 180));
+            endPos = transform.position;
         }
     }
 }
